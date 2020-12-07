c0ca2f37f21bbdcfe7b055df3227dd835aa6c305
Ubuntu <ubuntu@ip-172-31-56-5.ec2.internal>
2020-12-07 17:11:21 +0000

    linux setup

---
+++ linux_helpers.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#If the vim version isn't high enough on Ubuntu for YCM:
+
+sudo apt install -y vim-youcompleteme exuberant-ctags
+vim-addon-manager install youcompleteme
+
+sudo pip3 install black flake8 
+
+# Other options:
+
+# sudo add-apt-repository ppa:jonathonf/vim
+# sudo apt update
+# sudo apt install vim
+
+# If the gcc version isn't high enough:
+
+# sudo apt-get install gcc-8 g++-8
+# CXX=/usr/bin/gcc-8 ./install.py --all
+
